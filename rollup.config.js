2eda5cdf6b7dd3bc768d14845cad756d3612f401
nitaking <10850034+nitaking@users.noreply.github.com>
2019-07-07 20:15:58 +0900

    Fix: rollup config for flow

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import resolve from "rollup-plugin-node-resolve"
 import babel from "rollup-plugin-babel"
+import flow from "rollup-plugin-flow"
 import filesize from "rollup-plugin-filesize"
 import minify from "rollup-plugin-babel-minify"
 
@@ -18,6 +19,7 @@ export default {
         })
       : null,
     filesize(),
+    flow({ all: true }),
   ],
   external: [
     "reactotron-core-client",
