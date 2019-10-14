6feae06941ace05999c435cde7b178aa616137ff
rmevans9 <rmevans9@gmail.com>
2019-10-14 19:24:38 -0400

    feat: Allow for additional types of connections
    BREAKING CHANGES: This changes how communication works slightly. This should not cause any issues but we are marking it breaking just to be on the safe side.

---
+++ src/connection-manager.ts
@@ -0,0 +1,39 @@
+import { NativeModules } from "react-native"
+
+export default class ConnectionManager {
+  private webSocket: any
+  private flipperConnection: any
+
+  constructor(path?: string) {
+    this.webSocket = new WebSocket(path)
+    this.flipperConnection = NativeModules.Reactotron
+  }
+
+  send(payload: any) {
+    if (this.webSocket.readyState === 1) {
+      this.webSocket.send(payload)
+    }
+
+    if (this.flipperConnection) {
+      this.flipperConnection.sendCommand("command", JSON.parse(payload))
+    }
+  }
+
+  on(event: "open" | "close" | "message", callback: any) {
+    if (event === "open") {
+      if (this.flipperConnection) {
+        callback()
+      }
+
+      this.webSocket.onopen = callback
+    } else if (event === "close") {
+      this.webSocket.onclose = callback
+    } else if (event === "message") {
+      this.webSocket.onmessage = evt => callback(evt.data)
+    }
+  }
+
+  close() {
+    this.webSocket.close()
+  }
+}
