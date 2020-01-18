f112a2a41c2a1230d63594ba7ca0f93979c2f473
rmevans9 <rmevans9@gmail.com>
2020-01-18 10:36:13 -0500

    feat: Switch to FBs react native flipper plugin

--- src/connection-manager.ts
+++ src/connection-manager.ts
@@ -1,47 +1,21 @@
-import { NativeEventEmitter, NativeModules } from "react-native"
-
 export default class ConnectionManager {
   private webSocket: any
-  private flipperConnection: any
 
   constructor(path?: string) {
     this.webSocket = new WebSocket(path)
-    this.flipperConnection = NativeModules.Reactotron
   }
 
   send(payload: any) {
     if (this.webSocket.readyState === 1) {
       this.webSocket.send(payload)
     }
-
-    if (this.flipperConnection) {
-      this.flipperConnection.sendCommand("command", JSON.parse(payload))
-    }
   }
 
   on(event: "open" | "close" | "message", callback: any) {
     if (event === "open") {
-      if (this.flipperConnection) {
-        callback()
-      }
-
       this.webSocket.onopen = callback
-    } else if (event === "close") {
-      this.webSocket.onclose = () => {
-        if (!this.flipperConnection) {
-          callback()
-        }
-      }
     } else if (event === "message") {
       this.webSocket.onmessage = evt => callback(evt.data)
-
-      if (this.flipperConnection) {
-        const flipperConnectionEmitter = new NativeEventEmitter(NativeModules.Reactotron)
-
-        flipperConnectionEmitter.addListener("CommandReceived", command => {
-          callback(command)
-        })
-      }
     }
   }
 
